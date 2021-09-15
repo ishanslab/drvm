6f3d0ea39efedf6aab5cd2f284b6b2546d518904
ishanslab <90043870+ishanslab@users.noreply.github.com>
2021-09-15 22:23:53 +0530

    Add files via upload

---
+++ VMDRPE.tf
@@ -0,0 +1,193 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.76"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# VM Creation part starts here!!!
+# Ref: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_machine
+variable "prefix" {
+  default = "ish321"
+}
+
+resource "azurerm_resource_group" "primary" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "primary" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.primary.location
+  resource_group_name = azurerm_resource_group.primary.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.primary.name
+  virtual_network_name = azurerm_virtual_network.primary.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
+
+resource "azurerm_network_interface" "primary" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.primary.location
+  resource_group_name = azurerm_resource_group.primary.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "primary" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.primary.location
+  resource_group_name   = azurerm_resource_group.primary.name
+  network_interface_ids = [azurerm_network_interface.primary.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "P@ssword134!"
+  }
+  os_profile_linux_config  {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+# VM Creation part ends here!!!
+
+#--------------------------------------------------------------------------------
+
+#Recovery service vault part starts here!!!
+#ref: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/site_recovery_replicated_vm  
+
+resource "azurerm_resource_group" "secondary" {
+  name     = "tfex-replicated-vm-secondary"
+  location = "West Europe"
+}
+
+resource "azurerm_recovery_services_vault" "vault" {
+  name                = "example-recovery-vault"
+  location            = azurerm_resource_group.secondary.location
+  resource_group_name = azurerm_resource_group.secondary.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_site_recovery_fabric" "primary" {
+  name                = "primary-fabric"
+  resource_group_name = azurerm_resource_group.secondary.name
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+  location            = azurerm_resource_group.primary.location
+}
+
+resource "azurerm_site_recovery_fabric" "secondary" {
+  name                = "secondary-fabric"
+  resource_group_name = azurerm_resource_group.secondary.name
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+  location            = azurerm_resource_group.secondary.location
+}
+
+resource "azurerm_site_recovery_protection_container" "primary" {
+  name                 = "primary-protection-container"
+  resource_group_name  = azurerm_resource_group.secondary.name
+  recovery_vault_name  = azurerm_recovery_services_vault.vault.name
+  recovery_fabric_name = azurerm_site_recovery_fabric.primary.name
+}
+
+resource "azurerm_site_recovery_protection_container" "secondary" {
+  name                 = "secondary-protection-container"
+  resource_group_name  = azurerm_resource_group.secondary.name
+  recovery_vault_name  = azurerm_recovery_services_vault.vault.name
+  recovery_fabric_name = azurerm_site_recovery_fabric.secondary.name
+}
+
+resource "azurerm_site_recovery_replication_policy" "policy" {
+  name                                                 = "policy"
+  resource_group_name                                  = azurerm_resource_group.secondary.name
+  recovery_vault_name                                  = azurerm_recovery_services_vault.vault.name
+  recovery_point_retention_in_minutes                  = 24 * 60
+  application_consistent_snapshot_frequency_in_minutes = 4 * 60
+}
+
+resource "azurerm_site_recovery_protection_container_mapping" "container-mapping" {
+  name                                      = "container-mapping"
+  resource_group_name                       = azurerm_resource_group.secondary.name
+  recovery_vault_name                       = azurerm_recovery_services_vault.vault.name
+  recovery_fabric_name                      = azurerm_site_recovery_fabric.primary.name
+  recovery_source_protection_container_name = azurerm_site_recovery_protection_container.primary.name
+  recovery_target_protection_container_id   = azurerm_site_recovery_protection_container.secondary.id
+  recovery_replication_policy_id            = azurerm_site_recovery_replication_policy.policy.id
+}
+
+resource "azurerm_storage_account" "primary" {
+  name                     = "primaryrecoverycache"
+  location                 = azurerm_resource_group.primary.location
+  resource_group_name      = azurerm_resource_group.primary.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_site_recovery_replicated_vm" "vm-replication" {
+  name                                      = "vm-replication"
+  resource_group_name                       = azurerm_resource_group.secondary.name
+  recovery_vault_name                       = azurerm_recovery_services_vault.vault.name
+  source_recovery_fabric_name               = azurerm_site_recovery_fabric.primary.name
+  source_vm_id                              =  azurerm_virtual_machine.primary.id
+  recovery_replication_policy_id            = azurerm_site_recovery_replication_policy.policy.id
+  source_recovery_protection_container_name = azurerm_site_recovery_protection_container.primary.name
+
+  target_resource_group_id                = azurerm_resource_group.secondary.id
+  target_recovery_fabric_id               = azurerm_site_recovery_fabric.secondary.id
+  target_recovery_protection_container_id = azurerm_site_recovery_protection_container.secondary.id
+
+  managed_disk {
+    disk_id                    =  azurerm_virtual_machine.primary.storage_os_disk[0].managed_disk_id
+    staging_storage_account_id = azurerm_storage_account.primary.id
+    target_resource_group_id   = azurerm_resource_group.secondary.id
+    target_disk_type           = "Premium_LRS"
+    target_replica_disk_type   = "Premium_LRS"
+  }
+
+  network_interface {
+    source_network_interface_id   = azurerm_network_interface.primary.id
+    target_subnet_name            = azurerm_subnet.internal.name
+
+  }
+}
+
+#Recovery service vault part endss here!!!
